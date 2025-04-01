e5985f247e0993a180b6089ec1752ed3ba4403da
David Sambazov <sambazovd@gmail.com>
2025-04-01 13:12:32 +0200

    Nodemailer

--- config/env.js
+++ config/env.js
@@ -3,10 +3,10 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
-    PORT, NODE_ENV,
+    PORT, NODE_ENV, SERVER_URL,
     DB_URI,
     JWT_SECRET, JWT_EXPIRES_IN,
     ARCJET_ENV, ARCJET_KEY,
     QSTASH_TOKEN, QSTASH_URL,
-    SERVER_URL
+    EMAIL_PASSWORD,
 } = process.env;
